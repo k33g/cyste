4272de5613d49b288d5daa7be4c880cb03f37594
Ph. Charrière <pcharriere@gitlab.com>
2019-06-10 20:19:27 +0000

    :pencil: add some comments

--- cyste.js
+++ cyste.js
@@ -1,3 +1,11 @@
+/**
+  @license MIT
+  Copyright (c) 2019 Philippe Charrière
+  Twitter: @k33g_org
+  GitLab: @k33g
+  GitHub: @k33g
+*/
+
 window.cyste = {
   items: Object.entries(localStorage),
   getItem: (key) => localStorage.getItem(key),
